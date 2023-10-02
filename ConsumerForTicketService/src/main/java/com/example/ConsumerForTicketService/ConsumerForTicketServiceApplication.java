447b483d156ee176d2ebcf1b2a4caac3c19b2e2d
Aleksey Petkun <aleksey_2710@mail.ru>
2023-10-02 12:28:56 +0300

    initial commit

---
+++ ConsumerForTicketService/src/main/java/com/example/ConsumerForTicketService/ConsumerForTicketServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.ConsumerForTicketService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsumerForTicketServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsumerForTicketServiceApplication.class, args);
+	}
+
+}
---
+++ TransportTicketApp/src/main/java/com/github/alekseypetkun/TransportTicketApp/TransportTicketAppApplication.java
@@ -0,0 +1,13 @@
+package com.github.alekseypetkun.TransportTicketApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransportTicketAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransportTicketAppApplication.class, args);
+	}
+
+}
