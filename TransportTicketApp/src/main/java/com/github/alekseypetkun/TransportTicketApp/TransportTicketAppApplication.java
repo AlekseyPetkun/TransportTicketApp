43d3dc921a614a594f4d2434ca776cdd23e4e9e2
Aleksey Petkun <aleksey_2710@mail.ru>
2023-10-02 14:05:19 +0300

    creating app

--- TransportTicketApp/src/main/java/com/github/alekseypetkun/TransportTicketApp/TransportTicketAppApplication.java
+++ TransportTicketApp/src/main/java/com/github/alekseypetkun/TransportTicketApp/TransportTicketAppApplication.java
@@ -1,9 +1,15 @@
 package com.github.alekseypetkun.TransportTicketApp;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(
+		title = "TransportTicketsMicroservicesApplication",
+		description = "Веб-приложение для предоставления возможностей пользователям покупать транспортные билеты"
+))
 public class TransportTicketAppApplication {
 
 	public static void main(String[] args) {
